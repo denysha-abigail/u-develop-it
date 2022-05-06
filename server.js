283400cc07fc018fc79220925c23c252a50da2d7
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-06 03:46:09 -0400

    connected to mysql database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        // your MySQL password
+        password: 'ana0314*',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request (Not Found)
 // displays 404 response when user tries undefined endpoints at the server
