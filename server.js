2393de871cf6b0dd843bffaa908b159cf1eedf32
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-06 05:04:28 -0400

    created POST candidate route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -76,7 +77,6 @@ app.get('/api/candidate/:id', (req, res) => {
 // ? denotes a placeholder (making this a prepared statement that can execute the same sql statements repeatedly using different values in place of the placeholder)
 // the additional parameter afterwards provides values to use in place of the prepared statement's placeholders
 // equivalent to saying DELETE * FROM candidates WHERE id = 1 if the params value was 1
-
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
@@ -102,17 +102,46 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // CREATE a candidate
 // INSERT INTO to add values to candidates table that are assigned to the params const
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//                 VALUES (?, ?, ?, ?)`;
+            // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+            //                 VALUES (?, ?, ?, ?)`;
 // four placeholders must match four values in params (an array must be used) as well as the order
-// const params = [1, 'Ronald', 'Firebank', 1];
+            // const params = [1, 'Ronald', 'Firebank', 1];
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+            // db.query(sql, params, (err, result) => {
+            //     if (err) {
+            //         console.log(err);
+            //     }
+            //     console.log(result);
+            // });
+
+// CREATE a candidate
+// post() to insert candidate into the candidates table
+// used object destructuring to pull body property out of the req.body object
+app.post('/api/candidate', ({ body }, res) => {
+    // inputCheck module verifies that user info in the request can create a candidate; used to validate user data before the changes are inserted into the database
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ errors });
+        return;
+    }
+
+    // no need to specify id as mysql will autogenerate it for us
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?, ?, ?)`;
+    // user data collected in req.body
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // default response for any other request (Not Found)
 // displays 404 response when user tries undefined endpoints at the server
