5c24b6d39fb383f0925a44f3bfc3abd791879c98
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-06 03:41:51 -0400

    tested express.js connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// default response for any other request (Not Found)
+// displays 404 response when user tries undefined endpoints at the server
+// this is a catchall route and must be placed as the last route as it overrides all the others if placed before
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
