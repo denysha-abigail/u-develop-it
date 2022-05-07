0587f854d85919a1c24f2479f55eb3ee59dc2ce6
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-07 05:46:15 -0400

    created POST route for voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -38,4 +38,33 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// allow people to register through the app (POST)
+router.post('/voter', ({ body }, res) => {
+    // although the ? prepared statements will protect us from malicious data, we should also take into consideration preventing blank records from being created
+    // data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?, ?, ?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// allow people to update their email address (PUT)
+
+// allow people to deactivate their account (DELETE)
+
 module.exports = router;
