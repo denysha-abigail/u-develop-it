331a2a3a3527ef4455ba96d8985f8e65cb2cea45
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-07 05:35:35 -0400

    created GET route for individual voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -6,7 +6,8 @@ const inputCheck = require('../../utils/inputCheck');
 // GET route for voters
 // performs a SELECT * FROM voters and returns the rows on success or 500 if errors
 router.get('/voters', (req, res) => {
-    const sql = `SELECT * FROM voters`;
+    // rows can be sorted on retrieval simply by including an ORDER BY clause
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -16,6 +17,23 @@ router.get('/voters', (req, res) => {
         res.json({
             message: 'success',
             data: rows
+        });
+    });
+});
+
+// get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        } 
+        res.json({
+            message: 'success',
+            data: row
         });
     });
 });
